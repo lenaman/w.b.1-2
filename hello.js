ed9ced386c0ce4826fe96e3e001359ae15581c19
Lena Man <manlena01@gmail.com>
2024-10-08 10:25:30 -0400

    initial commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+//Example 1: declare and assign separately
+var greeting;  //declration
+greeting = "Hello" //assignment
+
+//Example 2:
+//declare and assign on the same line
+var greeting = "Hello";
+console.log(greeting);
