c5ef604c678d66fd29c29f1a96073a73eae6387a
Lena Man <manlena01@gmail.com>
2024-10-08 13:14:00 -0400

    new js

---
+++ exercise/new.js
@@ -0,0 +1,33 @@
+//Exeercise 1
+var customerId = "TTW123456";
+console.log(customerId);
+
+var personFirstName = "Lena"
+console.log(personFirstName);
+
+var personLastName ="Man"
+console.log(personLastName);
+
+var gender ="female"
+console.log(gender);
+
+var dateOfBirth ="Dec. 30"
+console.log(dateOfBirth);
+
+var driversLicense ="GBH12345"
+console.log(driversLicense);
+
+var autoPolicyNum ="123456"
+console.log(autoPolicyNum);
+
+
+//Exercise 3
+
+var firstName;    //decalaration
+firstName = "Lena"  //assignment
+
+var city;
+city = "London"
+
+var state;
+state = "n/a"
--- strict-mode.js
+++ strict-mode.js
@@ -1,5 +1,13 @@
 "use strict"
+
 var number;
 number = 10;
 numbers = 12;
-console.log(number):
+console.log(number);
+
+
+var customerId = "TTW123456";
+console.log(customerId);
+
+var personFirstName = "Lena Man";
+console.log(personFirstName);
