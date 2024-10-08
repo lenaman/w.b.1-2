b138472eac8cbcc00b52cf8c6222a3b9383f3bfb
Lena Man <manlena01@gmail.com>
2024-10-08 15:39:47 -0400

    rule of 72

---
+++ exercise/MathScripts/rule_of_72.js
@@ -0,0 +1,6 @@
+var interest = 5
+var initialValue = 67000
+var doubledValuedTime = 72/20
+
+var rule = "At a " + interest + " interest rate, your savings account will be worth " +  initialValue*2 + " in " + doubledValuedTime + " years "
+console.log(rule)
