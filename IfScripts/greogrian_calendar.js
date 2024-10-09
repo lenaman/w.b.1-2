123a51389b1576b1a1e1fc3820cc05863e8834d7
Lena Man <manlena01@gmail.com>
2024-10-09 15:55:32 -0400

    new commit

---
+++ IfScripts/greogrian_calendar.js
@@ -0,0 +1,12 @@
+var year1 = 1900;
+var year2 = 1950;
+var year3 = 1999;
+var year4 = 2000;
+var year5 = 2001;
+var year6 = 2012;
+
+if (year5 % 4 == 0) {
+  console.log("it is a leap year");
+} else {
+  console.log("this is not a leap year");
+}
--- IfScripts/pay_rules.js
+++ IfScripts/pay_rules.js
@@ -1,14 +1,12 @@
-var payRate = 17.30;
+var payRate = 17.3;
 var hoursWorked = 45;
 
-let grossPay = 0
+let grossPay = 0;
 
-if (hoursWorked < 40) 
-//    let overtimeTimeHours = hoursWorked
-{grossPay = payRate*hoursWorked}
-else {
-    grossPay = payRate * 40 + (hoursWorked - 40) * payRate * 1.5
+if (hoursWorked < 40) {
+  //    let overtimeTimeHours = hoursWorked
+  grossPay = payRate * hoursWorked;
+} else {
+  grossPay = payRate * 40 + (hoursWorked - 40) * payRate * 1.5;
 }
-console.log(grossPay)
-
-
+console.log(grossPay);
