aa2cb6f9fc21a1eb6a03a6c83b5e5ce2715f9173
Lena Man <manlena01@gmail.com>
2024-10-10 10:42:01 -0400

    commit

---
+++ IfScripts/greeting.js
@@ -0,0 +1,9 @@
+var hour = 12
+
+if (hour < 10 ){
+    console.log("Good Morning!")
+}else if (hour < 17 ) {
+    console.log ("Good Day!")
+}else if (hour >=17) {
+    console.log("Good Evening!")
+}
