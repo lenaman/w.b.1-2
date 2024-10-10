493ef96b54a1a106877ac068113b720ba47c2d59
Lena Man <manlena01@gmail.com>
2024-10-10 10:17:14 -0400

    new

---
+++ IfScripts/even-odd.js
@@ -0,0 +1,21 @@
+var number = 4
+
+if (number % 2 ==0) {
+    console.log("The number is even")
+}else{
+    console.log("The number is odd")
+}
+
+// example from class//
+
+// var number = 4;
+// var condition = number > 5;
+
+// if (condition) {
+//   console.log("The number is greater than 5");
+//   console.log("hi");
+//   console.log("there");
+
+// } else {
+//   console.log("always happens");
+// }
--- IfScripts/pay_rules.js
+++ IfScripts/pay_rules.js
@@ -11,16 +11,4 @@ if (hoursWorked < 40) {
 }
 console.log(grossPay);
 
-// example from class//
 
-// var number = 4;
-// var condition = number > 5;
-
-// if (condition) {
-//   console.log("The number is greater than 5");
-//   console.log("hi");
-//   console.log("there");
-
-// } else {
-//   console.log("always happens");
-// }
