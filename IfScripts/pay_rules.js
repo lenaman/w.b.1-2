38c3fe3ee6eafbf9114f5ab13ba4087794cb199b
Lena Man <manlena01@gmail.com>
2024-10-10 09:18:02 -0400

    examples

--- IfScripts/pay_rules.js
+++ IfScripts/pay_rules.js
@@ -4,9 +4,24 @@ var hoursWorked = 45;
 let grossPay = 0;
 
 if (hoursWorked < 40) {
-  //    let overtimeTimeHours = hoursWorked
+     let overtimeTimeHours = hoursWorked
   grossPay = payRate * hoursWorked;
 } else {
   grossPay = payRate * 40 + (hoursWorked - 40) * payRate * 1.5;
 }
 console.log(grossPay);
+
+
+// example from class//
+
+// var number = 4;    
+// var condition = number > 5;
+
+// if (condition) {
+//   console.log("The number is greater than 5");
+//   console.log("hi");
+//   console.log("there");
+  
+// } else {
+//   console.log("always happens");
+// }
